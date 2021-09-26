047c1a8fe6827d4232b92ccb08addefd4e0d0b64
Shaun Kent <shaunkent81@gmail.com>
2021-09-25 20:55:19 -0700

    add Index controller, new form controller, httppost to index

---
+++ VendorOrder/Controller/VendorController.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using VendorOrder.Models;
+
+namespace VendorOrder.Controllers
+{
+  public class VendorController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(vendorName, vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
+
+    []
+  }
+}
