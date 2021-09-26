10bbc59a69de9e3dc6c21651553955c93f34dd8e
Shaun Kent <shaunkent81@gmail.com>
2021-09-25 18:12:33 -0700

    setup vendor.cs file, good fail for first test

--- VendorOrder/Models/Vendor.cs
+++ VendorOrder/Models/Vendor.cs
@@ -1,3 +1,17 @@
 using System.Collections.Generic;
 
 namespace VendorOrder.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Name { get; set; };
+    public int Id { get; }
+    public List<Item> Items { get; set; }
+
+    public Vendor(string vendorName)
+    {
+      
+    }
+  }
+}
