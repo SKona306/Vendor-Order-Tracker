f76f017a3f21458aa55d32b4665684398b610f8e
Shaun Kent <shaunkent81@gmail.com>
2021-09-25 18:26:16 -0700

    finish creating constructor with properties

--- VendorOrder/Models/Vendor.cs
+++ VendorOrder/Models/Vendor.cs
@@ -5,13 +5,14 @@ namespace VendorOrder.Models
   public class Vendor
   {
     private static List<Vendor> _instances = new List<Vendor> {};
-    public string Name { get; set; };
+    public string Name { get; set; }
     public int Id { get; }
-    public List<Item> Items { get; set; }
 
     public Vendor(string vendorName)
     {
-      
+      Name = vendorName;
+      _instances.Add(this);
+      Id = _instances.Count;
     }
   }
 }
