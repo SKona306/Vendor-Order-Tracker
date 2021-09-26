49fedbff6d942f6782e9be1bca7872e2adba1bf5
Shaun Kent <shaunkent81@gmail.com>
2021-09-25 21:42:03 -0700

    add controller for Show page on Orders class

--- VendorOrder/Controller/OrderController.cs
+++ VendorOrder/Controller/OrderController.cs
@@ -13,12 +13,22 @@ namespace VendorOrder.Controllers
       return View(vendor);
     }
 
-    [HttpPost("/items/delete")]
+    [HttpPost("/orders/delete")]
     public ActionResult DeleteAll()
     {
       Order.ClearAll();
       return View();
     }
 
+    [HttpGet("vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> model = new Dictionary<string, object> ();
+      model.Add("order", order);
+      model.Add("vendor", vendor);
+      return View(model);
+    }
   }
 }
