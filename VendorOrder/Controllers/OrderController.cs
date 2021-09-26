9e426da91d7f21ff17ed56b1a2fbcd3251128e78
Shaun Kent <shaunkent81@gmail.com>
2021-09-26 01:20:42 -0700

    change syntax of keys

--- VendorOrder/Controllers/OrderController.cs
+++ VendorOrder/Controllers/OrderController.cs
@@ -26,8 +26,8 @@ namespace VendorOrder.Controllers
       Order order = Order.Find(orderId);
       Vendor vendor = Vendor.Find(vendorId);
       Dictionary<string, object> model = new Dictionary<string, object> ();
-      model.Add("order", order);
-      model.Add("vendor", vendor);
+      model.Add("orders", order);
+      model.Add("vendors", vendor);
       return View(model);
     }
   }
