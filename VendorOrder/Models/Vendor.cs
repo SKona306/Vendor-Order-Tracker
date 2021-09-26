d5444a25296481e8c72a7beb3c50b4b4084cb039
Shaun Kent <shaunkent81@gmail.com>
2021-09-25 20:30:55 -0700

    add AddOrder method, add order list to vendor constructor

--- VendorOrder/Models/Vendor.cs
+++ VendorOrder/Models/Vendor.cs
@@ -8,6 +8,7 @@ namespace VendorOrder.Models
     public string Name { get; set; }
     public string Description { get; set; }
     public int Id { get; }
+    public List<Order> Orders { get; set; }
 
     public Vendor(string vendorName, string vendorDescription)
     {
@@ -15,6 +16,7 @@ namespace VendorOrder.Models
       Description = vendorDescription;
       _instances.Add(this);
       Id = _instances.Count;
+      Orders = new List<Order> {};
     }
 
     public static void ClearAll()
@@ -30,6 +32,11 @@ namespace VendorOrder.Models
     public static Vendor Find(int searchId)
     {
       return _instances[searchId-1];
+    }
+
+    public void AddOrder(Order order)
+    {
+      Orders.Add(order);
     }
   }
 }
