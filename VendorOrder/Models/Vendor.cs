62c79de39fb87100e4d2d474fdb7e4b59c0cbf38
Shaun Kent <shaunkent81@gmail.com>
2021-09-25 18:32:11 -0700

    add description to vendor constructor

--- VendorOrder/Models/Vendor.cs
+++ VendorOrder/Models/Vendor.cs
@@ -6,11 +6,13 @@ namespace VendorOrder.Models
   {
     private static List<Vendor> _instances = new List<Vendor> {};
     public string Name { get; set; }
+    public string Description { get; set; }
     public int Id { get; }
 
-    public Vendor(string vendorName)
+    public Vendor(string vendorName, string vendorDescription)
     {
       Name = vendorName;
+      Description = vendorDescription;
       _instances.Add(this);
       Id = _instances.Count;
     }
