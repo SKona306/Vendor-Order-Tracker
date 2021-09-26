f1c5b2f1c8d2486218c3cbe0221f7be874e839d4
Shaun Kent <shaunkent81@gmail.com>
2021-09-25 19:25:50 -0700

    add second test for Order class

--- VendorOrder.Tests/ModelTests/OrderTests.cs
+++ VendorOrder.Tests/ModelTests/OrderTests.cs
@@ -14,5 +14,23 @@ namespace VendorOrder.Tests
       Order newOrder = new Order("test", "this is a test", "10", "may 10th");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrderAndReturnsCorrectProperties_True()
+    {
+      string title = "A Title";
+      string description = "This is a test";
+      string date = "May 1st 2021";
+      string price = "23";
+      Order newOrder = new Order(title, description, date, price);
+      string resultTitle = newOrder.Title;
+      string resultDescription = newOrder.Description;
+      string resultDate = newOrder.Date;
+      string resultPrice = newOrder.Price;
+      Assert.AreEqual(title, resultTitle);
+      Assert.AreEqual(description, resultDescription);
+      Assert.AreEqual(date, resultDate);
+      Assert.AreEqual(price, resultPrice);
+    }
   }
 }
